8acf53838b3689796c87158f2b9c7ceb09ad4a58
Thiago <thiago@tecnonutri-mac-mini.local>
2015-09-15 10:19:07 -0300

    Initial Commit

---
+++ TapCounter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TapCounter
+//
+//  Created by Thiago on 9/15/15.
+//  Copyright (c) 2015 Thiago. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TapCounter/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TapCounter
+//
+//  Created by Thiago on 9/15/15.
+//  Copyright (c) 2015 Thiago. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
